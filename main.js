e9bc2c48b6249f45d77366f5272f437a211db88c
William Gomez <gome4810@bellsouth.net>
2018-11-14 18:16:17 -0500

    My first commit

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ $(document).ready(function(){
         }
          });
 });
+
+$('.galeria--img').click(function(e){
+  var img = e.target.src;
+  var modal = '<div class="modal" id="modal" ><img src=" '+ img +'" class="modal--img"><div class="modal--boton" id="modal--boton">x</div></div>';
+  $('body').append(modal);
+  $('#modal--boton').click(function(){
+      $('#modal').remove();
+  })
+})
